c490182f72b262ac77aa908d88792e2a65ac80c0
Deniz Gençtürk <denizar@gmail.com>
2022-11-29 16:42:56 +0100

    fix: await redis connection

--- src/index.js
+++ src/index.js
@@ -7,41 +7,46 @@ const express = require('express')
 const app = express()
 
 const config = require('./config')
-const { pubSubAdapter } = require('./redis')
+const { awaitConnection, pubSubAdapter } = require('./redis')
 const { port, serverURL } = config
-const parseServer = new ParseServer(config)
 
-app.use('/parse', parseServer.app)
-app.get('/healthz', async (req, res) => {
-  const { data } = await Parse.Cloud.httpRequest({
-    url: serverURL + '/health',
-    headers: {
-      'X-Parse-Application-Id': process.env.APP_ID,
-      'X-Parse-MASTER-key': process.env.MASTER_KEY,
-      'Content-Type': 'application/json'
-    }
-  })
-  return res.send(data)
-})
+const initApp = async () => {
+  await awaitConnection()
 
-if (process.env.NODE_ENV === 'development') {
-  const Dashboard = require('parse-dashboard')
-  app.use('/dashboard', new Dashboard({
-    apps: [
-      {
-        serverURL,
-        appId: process.env.APP_ID,
-        masterKey: process.env.MASTER_KEY,
-        appName: process.env.APP_NAME
+  const parseServer = new ParseServer(config)
+  app.use('/parse', parseServer.app)
+  app.get('/healthz', async (req, res) => {
+    const { data } = await Parse.Cloud.httpRequest({
+      url: serverURL + '/health',
+      headers: {
+        'X-Parse-Application-Id': process.env.APP_ID,
+        'X-Parse-MASTER-key': process.env.MASTER_KEY,
+        'Content-Type': 'application/json'
       }
-    ]
-  }))
-}
+    })
+    return res.send(data)
+  })
 
-const httpServer = require('http').createServer(app)
-httpServer.listen(port, function () {
-  consola.success(`${process.env.APP_NAME} Parse Server is running on ${serverURL}`)
-  ParseServer.createLiveQueryServer(httpServer, { pubSubAdapter })
-  consola.success(`${process.env.APP_NAME} Parse LiveQueryServer running on ws://localhost:${port}`)
-  process.env.NODE_ENV === 'development' && consola.success(`${process.env.APP_NAME} Parse Dashboard is running on http://localhost:${port}/dashboard`)
-})
+  if (process.env.NODE_ENV === 'development') {
+    const Dashboard = require('parse-dashboard')
+    app.use('/dashboard', new Dashboard({
+      apps: [
+        {
+          serverURL,
+          appId: process.env.APP_ID,
+          masterKey: process.env.MASTER_KEY,
+          appName: process.env.APP_NAME
+        }
+      ]
+    }))
+  }
+
+  const httpServer = require('http').createServer(app)
+  httpServer.listen(port, function () {
+    consola.success(`${process.env.APP_NAME} Parse Server is running on ${serverURL}`)
+    ParseServer.createLiveQueryServer(httpServer, { pubSubAdapter })
+    consola.success(`${process.env.APP_NAME} Parse LiveQueryServer running on ws://localhost:${port}`)
+    process.env.NODE_ENV === 'development' && consola.success(`${process.env.APP_NAME} Parse Dashboard is running on http://localhost:${port}/dashboard`)
+  })
+}
+initApp()
--- src/redis.js
+++ src/redis.js
@@ -7,57 +7,62 @@ const getNodes = () => process.env.REDIS_NODES.split(',').map(node => {
 })
 
 const getRedisOptions = ({ db } = {}) => {
-  if (process.env.url) { return process.env.url }
   if (!db) { db = process.env.REDIS_DB || 0 }
 
-  if (process.env.REDIS_MODE === 'cluster') {
-    if (!process.env.REDIS_NODES) {
-      throw new Error('REDIS_NODES is not defined')
-    }
+  if (process.env.REDIS_SENTINELS) {
     return {
-      redisOptions: {
-        password: process.env.REDIS_PASSWORD,
-        username: process.env.REDIS_USERNAME,
-        maxRetriesPerRequest: null
-      },
-      enableReadyCheck: false,
-      enableAutoPipelining: true,
-      enableOfflineQueue: false,
-      keyPrefix: process.env.APP_ID
+      sentinels: process.env.REDIS_SENTINELS.split(',').map((sentinel) => {
+        const [host, port] = sentinel.split(':')
+        return { host, port }
+      }),
+      name: process.env.REDIS_NAME,
+      password: process.env.REDIS_PASS,
+      db
     }
   }
-  if (process.env.REDIS_MODE === 'sentinel') {
-    if (!process.env.REDIS_NODES) {
-      throw new Error('REDIS_NODES is not defined')
-    }
-    return {
-      sentinels: getNodes(),
-      name: process.env.REDIS_MASTER_NAME ?? 'mymaster',
-      password: process.env.REDIS_PASSWORD,
-      username: process.env.REDIS_USERNAME,
-      db: process.env.REDIS_DB ?? 0,
-      enableReadyCheck: false,
-      maxRetriesPerRequest: null,
-      enableOfflineQueue: false,
-      keyPrefix: process.env.APP_ID
-    }
+  if (process.env.REDIS_MODE === 'cluster') {
+    throw new Error('get redis options should not run in cluster mode')
   }
   return {
     host: process.env.REDIS_HOST ?? 'localhost',
     port: process.env.REDIS_PORT ?? 6379,
-    password: process.env.REDIS_PASSWORD,
-    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASS,
+    username: process.env.REDIS_USER,
     db,
     enableReadyCheck: false,
-    maxRetriesPerRequest: null,
-    keyPrefix: process.env.APP_ID
+    maxRetriesPerRequest: null
   }
 }
 
+const getClusterOptions = () => ({
+  redisOptions: {
+    password: process.env.REDIS_PASS,
+    username: process.env.REDIS_USER,
+    maxRetriesPerRequest: null
+  },
+  enableReadyCheck: false,
+  enableAutoPipelining: true,
+  enableOfflineQueue: false,
+  natMap: process.env.NODE_ENV === 'development'
+    ? {
+      '10.5.0.2:6479': { host: '127.0.0.1', port: 6479 },
+      '10.5.0.3:6479': { host: '127.0.0.1', port: 6480 },
+      '10.5.0.4:6479': { host: '127.0.0.1', port: 6481 },
+      '10.5.0.5:6479': { host: '127.0.0.1', port: 6482 },
+      '10.5.0.6:6479': { host: '127.0.0.1', port: 6483 },
+      '10.5.0.7:6479': { host: '127.0.0.1', port: 6487 }
+    }
+    : undefined
+})
+
+const getRedisClient = () => process.env.REDIS_MODE === 'cluster'
+  ? new Redis.Cluster(getNodes(), getClusterOptions())
+  : new Redis(getRedisOptions())
+
 class PubSubAdapter {
-  constructor (config) {
-    this.pub = new Redis(config)
-    this.sub = new Redis(config)
+  constructor () {
+    this.pub = getRedisClient()
+    this.sub = getRedisClient()
   }
 
   createPublisher () {
@@ -69,13 +74,16 @@ class PubSubAdapter {
   }
 }
 
-const getRedisClient = () => process.env.REDIS_MODE === 'cluster'
-  ? new Redis.Cluster(getNodes(), getRedisOptions())
-  : new Redis(getRedisOptions())
-
 const redis = getRedisClient()
 
+const awaitConnection = async () => {
+  while (redis.status !== 'ready') {
+    await new Promise(resolve => setTimeout(resolve, 100))
+  }
+}
+
 module.exports = redis
+module.exports.awaitConnection = awaitConnection
 module.exports.getRedisOptions = getRedisOptions
 module.exports.getRedisClient = getRedisClient
-module.exports.pubSubAdapter = new PubSubAdapter(getRedisOptions())
+module.exports.pubSubAdapter = new PubSubAdapter()
