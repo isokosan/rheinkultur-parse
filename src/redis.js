220a0c5e6b185eed1601284d6e8b6e7ed23f1b94
Deniz Gençtürk <denizar@gmail.com>
2022-11-29 13:00:10 +0100

    fix: add semantics

--- src/redis.js
+++ src/redis.js
@@ -1,6 +1,11 @@
 // https://github.com/redis/node-redis/blob/master/docs/client-configuration.md
 const Redis = require('ioredis')
 
+const getNodes = () => process.env.REDIS_NODES.split(',').map(node => {
+  const [host, port] = node.split(':')
+  return { host, port }
+})
+
 const getRedisOptions = ({ db } = {}) => {
   if (process.env.url) { return process.env.url }
   if (!db) { db = process.env.REDIS_DB || 0 }
@@ -10,10 +15,6 @@ const getRedisOptions = ({ db } = {}) => {
       throw new Error('REDIS_NODES is not defined')
     }
     return {
-      nodes: process.env.REDIS_NODES.split(',').map(node => {
-        const [host, port] = node.split(':')
-        return { host, port }
-      }),
       redisOptions: {
         password: process.env.REDIS_PASSWORD,
         username: process.env.REDIS_USERNAME,
@@ -30,10 +31,7 @@ const getRedisOptions = ({ db } = {}) => {
       throw new Error('REDIS_NODES is not defined')
     }
     return {
-      sentinels: process.env.REDIS_NODES.split(',').map(node => {
-        const [host, port] = node.split(':')
-        return { host, port }
-      }),
+      sentinels: getNodes(),
       name: process.env.REDIS_MASTER_NAME ?? 'mymaster',
       password: process.env.REDIS_PASSWORD,
       username: process.env.REDIS_USERNAME,
@@ -71,8 +69,13 @@ class PubSubAdapter {
   }
 }
 
-const redis = new Redis(getRedisOptions())
+const getRedisClient = () => process.env.REDIS_MODE === 'cluster'
+  ? new Redis.Cluster(getNodes(), getRedisOptions())
+  : new Redis(getRedisOptions())
+
+const redis = getRedisClient()
 
 module.exports = redis
 module.exports.getRedisOptions = getRedisOptions
+module.exports.getRedisClient = getRedisClient
 module.exports.pubSubAdapter = new PubSubAdapter(getRedisOptions())
