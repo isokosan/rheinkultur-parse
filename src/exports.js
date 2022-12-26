375823cf1e731067363ac62033cc9a1b6e32a45f
Deniz Gençtürk <denizar@gmail.com>
2022-12-26 18:42:28 +0100

    chore: add dummy stream export example

--- src/exports.js
+++ src/exports.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const router = express.Router()
+
+const excel = require('exceljs')
 const handleErrorAsync = func => (req, res, next) => func(req, res, next).catch((error) => next(error))
 
-router.get('/', handleErrorAsync((req, res) => {
-  return res.send('ok')
+router.get('/', handleErrorAsync(async (req, res) => {
+  const workbook = new excel.stream.xlsx.WorkbookWriter({})
+  const worksheet = workbook.addWorksheet('Sheetname')
+  await worksheet.commit()
+  const buffer = workbook.stream.read()
+  const filename = 'Filename'
+  res.set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+  res.set(`Content-Disposition', 'attachment; filename=${filename}.xlsx`)
+  res.set('Content-Length', buffer.length)
+  return res.send(buffer)
 }))
 
 module.exports = router
