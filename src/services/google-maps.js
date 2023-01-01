64e07b0a2355edd76f7461300039c5148c5a5798
Deniz Gençtürk <denizar@gmail.com>
2023-01-01 17:13:19 +0100

    chore: add google and google-maps services

---
+++ src/services/google-maps.js
@@ -0,0 +1,21 @@
+const { Client } = require('@googlemaps/google-maps-services-js')
+const client = new Client({})
+const key = process.env.GOOGLE_MAPS_API_KEY
+
+const getPlacesPredictions = async (input) => {
+  const { data: { predictions } } = await client.placeAutocomplete({
+    params: { key, input, language: 'de', region: 'de' }
+  })
+  return predictions
+}
+
+const getPlaceById = async (place_id) => {
+  const { data: { result: place } } = await client.placeDetails({
+    params: { key, place_id }
+  })
+  return place
+}
+
+module.exports = client
+module.exports.getPlacesPredictions = getPlacesPredictions
+module.exports.getPlaceById = getPlaceById
