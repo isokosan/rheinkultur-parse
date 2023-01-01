6db68e4362aa4cca83ccb21c973a008ddd15ea7a
Deniz Gençtürk <denizar@gmail.com>
2023-01-01 18:56:23 +0100

    fix: Use json key for google auth

--- src/services/googleapis.js
+++ src/services/googleapis.js
@@ -1,17 +1,12 @@
 const { google } = require('googleapis')
-const { readFileSync } = require('fs')
 
-const scopes = [
-  'https://www.googleapis.com/auth/documents',
-  'https://www.googleapis.com/auth/drive'
-]
-
-const auth = new google.auth.JWT(
-  process.env.GOOGLE_APPLICATION_CLIENT_EMAIL,
-  null,
-  readFileSync('./.googlekey', 'utf-8'),
-  scopes
-)
+const auth = new google.auth.GoogleAuth({
+  keyFile: './.googlekey',
+  scopes: [
+    'https://www.googleapis.com/auth/documents',
+    'https://www.googleapis.com/auth/drive'
+  ]
+})
 
 const drive = google.drive({ version: 'v3', auth })
 const docs = google.docs({ version: 'v1', auth })
