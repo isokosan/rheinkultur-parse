7fa53a63208a12d97a6489943604b5be57c83a31
Deniz Gençtürk <denizar@gmail.com>
2023-05-12 14:44:25 +0300

    chore: permissions update

--- updates/set-permissions.js
+++ updates/set-permissions.js
@@ -6,18 +6,52 @@ console.log(Parse.serverURL)
 Parse.initialize(process.env.APP_ID, process.env.JAVASCRIPT_KEY, process.env.MASTER_KEY)
 require('./../src/globals')
 
-// set manage-fieldwork permissions
-const manageFieldwork = [
-  'rwe@rheinkultur-medien.de',
-  'denizar@gmail.com',
-  'sth@rheinkultur-medien.de',
-  'giwe@rheinkultur-medien.de'
-]
-$query(Parse.User)
-  .containedIn('username', manageFieldwork)
-  .notEqualTo('permissions', 'manage-fieldwork')
-  .each((user) => {
-    const permissions = user.get('permissions') || []
-    permissions.push('manage-fieldwork')
-    return user.set({ permissions }).save(null, { useMasterKey: true })
-  }, { useMasterKey: true })
+async function setPermissions () {
+  let i = 0
+  const manageFieldwork = [
+    'rwe@rheinkultur-medien.de',
+    'denizar@gmail.com',
+    'sth@rheinkultur-medien.de',
+    'giwe@rheinkultur-medien.de'
+  ]
+  await $query(Parse.User)
+    .containedIn('username', manageFieldwork)
+    .notEqualTo('permissions', 'manage-fieldwork')
+    .each((user) => {
+      const permissions = user.get('permissions') || []
+      permissions.push('manage-fieldwork')
+      i++
+      return user.set({ permissions }).save(null, { useMasterKey: true })
+    }, { useMasterKey: true })
+
+  const manageScouts = [
+    'rwe@rheinkultur-medien.de',
+    'sth@rheinkultur-medien.de',
+    'marc@asriel.de'
+  ]
+  await $query(Parse.User)
+    .containedIn('username', manageScouts)
+    .notEqualTo('permissions', 'manage-scouts')
+    .each((user) => {
+      const permissions = user.get('permissions') || []
+      permissions.push('manage-scouts')
+      i++
+      return user.set({ permissions }).save(null, { useMasterKey: true })
+    }, { useMasterKey: true })
+
+  const manageBookings = [
+    'marc@asriel.de'
+  ]
+  await $query(Parse.User)
+    .containedIn('username', manageBookings)
+    .notEqualTo('permissions', 'manage-bookings')
+    .each((user) => {
+      const permissions = user.get('permissions') || []
+      permissions.push('manage-bookings')
+      i++
+      return user.set({ permissions }).save(null, { useMasterKey: true })
+    }, { useMasterKey: true })
+  return i
+}
+
+setPermissions().then(consola.success)
