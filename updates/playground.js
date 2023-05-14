e2abcf8266eb50a6765d6e7f443b24a0a85e60e3
Deniz Gençtürk <denizar@gmail.com>
2023-05-14 15:33:18 +0300

    chore: playground

---
+++ updates/playground.js
@@ -0,0 +1,7 @@
+// require('dotenv').config()
+// global.Parse = require('parse/node')
+// Parse.serverURL = process.env.PRODUCTION_SERVER_URL
+// // Parse.serverURL = process.env.PUBLIC_SERVER_URL
+// console.log(Parse.serverURL)
+// Parse.initialize(process.env.APP_ID, process.env.JAVASCRIPT_KEY, process.env.MASTER_KEY)
+// require('./../src/globals')
