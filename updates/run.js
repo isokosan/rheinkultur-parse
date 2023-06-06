917c8509519a1137d8b4941cf919d37a41b9c2bc
isokosan <denizar@gmail.com>
2023-06-06 09:45:44 +0200

    chore: fix run fn

--- updates/run.js
+++ updates/run.js
@@ -1,28 +1,34 @@
 require('dotenv').config()
 global.Parse = require('parse/node')
 require('./../src/globals')
+global.DEVELOPMENT = process.argv[2] !== '--prod'
 
 module.exports = async (func) => {
-  const isProduction = process.argv[2] === '--prod'
-  function ask () {
-    process.stdout.write('The process will run on the production server. Proceed? (y/n)?')
-  }
-  if (isProduction) {
-    process.stdin.on('data', function (data) {
-      if (data.toString().trim() === 'y') {
-        Parse.serverURL = process.env.PRODUCTION_SERVER_URL
-        Parse.initialize(process.env.APP_ID, process.env.JAVASCRIPT_KEY, process.env.MASTER_KEY)
-        return func()
-      }
-      if (data.toString().trim() === 'n') {
-        return process.stdin.end()
-      }
-      return ask()
-    })
-    return ask()
-  }
-  // DEVELOPMENT
-  Parse.serverURL = process.env.PUBLIC_SERVER_URL
+  Parse.serverURL = DEVELOPMENT ? process.env.PUBLIC_SERVER_URL : process.env.PRODUCTION_SERVER_URL
   Parse.initialize(process.env.APP_ID, process.env.JAVASCRIPT_KEY, process.env.MASTER_KEY)
-  return func()
+  async function runAndExit () {
+    consola.info(`Running ${func.name} on ${Parse.serverURL}`)
+    try {
+      await func()
+      process.exit()
+    } catch (e) {
+      console.log(e)
+      process.exit()
+    }
+  }
+  function ask () {
+    return new Promise((resolve) => {
+      process.stdin.once('data', function (data) {
+        if (data.toString().trim() === 'y') {
+          return runAndExit()
+        }
+        if (data.toString().trim() === 'n') {
+          return process.exit()
+        }
+        return ask()
+      })
+      process.stdout.write('The process will run on the production server. OK? (y/n)')
+    })
+  }
+  return DEVELOPMENT ? runAndExit() : ask()
 }
