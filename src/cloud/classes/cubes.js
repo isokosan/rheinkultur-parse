31d64489777fad046a3a3a94ba426d74c49f2bb3
isokosan <denizar@gmail.com>
2023-06-07 22:53:57 +0200

    fix: remove ht

--- src/cloud/classes/cubes.js
+++ src/cloud/classes/cubes.js
@@ -258,7 +258,7 @@ Parse.Cloud.define('cube-update-media', async ({ params: { id, media }, user }) 
 
 Parse.Cloud.define('cube-update-ht', async ({ params: { id, housingTypeId }, user }) => {
   const cube = await $getOrFail(Cube, id)
-  const ht = await $getOrFail('HousingType', housingTypeId)
+  const ht = housingTypeId ? await $getOrFail('HousingType', housingTypeId) : null
   const changes = { htId: [cube.get('ht')?.id, housingTypeId] }
   housingTypeId
     ? cube.set({ ht, media: ht.get('media') })
