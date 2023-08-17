d055ec8138da3fdd6e34d8560c39c0787294302a
isokosan <denizar@gmail.com>
2023-08-17 13:55:15 +0200

    fix: webp rotation bug

--- src/cloud/storage.js
+++ src/cloud/storage.js
@@ -28,10 +28,11 @@ const getThumbnail = async (file) => {
   // try to generate thumb if image
   return sharp(Buffer.from(await file.getData(), 'base64'))
     .resize({ height: 270, width: 270, fit: sharp.fit.inside })
-    // .webp({ nearLossless: true }) !! withMetadata does not work and causes bugs with mobile upload orientation
+    .webp({ nearLossless: true })
+    .rotate() // !! withMetadata after webp does not get orientation so rotate fixes that
     .withMetadata()
     .toBuffer()
-    .then(data => new Parse.File('thumb_' + file._name, { base64: data.toString('base64') }, undefined, { thumb: 'true' }))
+    .then(data => new Parse.File('thumb.webp', { base64: data.toString('base64') }, 'image/webp', { thumb: 'true' }))
     .catch((error) => {
       if (error.message !== 'Input buffer contains unsupported image format') {
         consola.error(error.code, error.message)
@@ -44,7 +45,8 @@ const getSize1000 = async (file) => {
   if (file._metadata.thumb) { return }
   const base64 = await sharp(Buffer.from(await file.getData(), 'base64'))
     .resize({ height: 1000, width: 1000, fit: sharp.fit.inside })
-    // .webp({ nearLossless: true }) !! withMetadata does not work and causes bugs with mobile upload orientation
+    .webp({ nearLossless: true })
+    .rotate() // !! withMetadata after webp does not get orientation so rotate fixes that
     .withMetadata()
     .toBuffer()
     .then(data => data.toString('base64'))
@@ -52,7 +54,7 @@ const getSize1000 = async (file) => {
       handleFileDestroyError(error)
       return null
     })
-  return new Parse.File('size1000_' + file._name, { base64 }, undefined, { thumb: 'size1000' })
+  return new Parse.File('size1000.webp', { base64 }, 'image/webp', { thumb: 'size1000' })
 }
 
 Parse.Cloud.beforeSaveFile(async ({ file }) => {
