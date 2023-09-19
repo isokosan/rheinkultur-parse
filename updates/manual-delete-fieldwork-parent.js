f8156872bfafc17da3d1bc288f8ded89210859a5
isokosan <denizar@gmail.com>
2023-09-19 16:36:15 +0200

    wip: control reports

--- updates/manual-delete-fieldwork-parent.js
+++ updates/manual-delete-fieldwork-parent.js
@@ -1,6 +1,6 @@
 async function clean () {
   // briefings
-  await $query('Briefing').containedIn('objectId', []).each(async (briefing) => {
+  await $query('Briefing').containedIn('objectId', ['8X6GK15zZm']).each(async (briefing) => {
     // delete all submissions
     const taskListQuery = $query('TaskList').equalTo('briefing', briefing)
     await $query('ScoutSubmission').matchesQuery('taskList', taskListQuery).each(async (submission) => {
@@ -14,37 +14,37 @@ async function clean () {
   }, { useMasterKey: true })
 
   // controls
-  await $query('Control').containedIn('objectId', ['056ABySF2a']).each(async (control) => {
-    // delete all submissions
-    const taskListQuery = $query('TaskList').equalTo('control', control)
-    await $query('ControlSubmission').matchesQuery('taskList', taskListQuery).each(async (submission) => {
-      await submission.destroy({ useMasterKey: true })
-    }, { useMasterKey: true })
-    await taskListQuery.each(async (taskList) => {
-      await taskList.destroy({ useMasterKey: true })
-    }, { useMasterKey: true })
-    await control.destroy({ useMasterKey: true })
-    console.info('Control removed')
-  }, { useMasterKey: true })
+  // await $query('Control').containedIn('objectId', []).each(async (control) => {
+  //   // delete all submissions
+  //   const taskListQuery = $query('TaskList').equalTo('control', control)
+  //   await $query('ControlSubmission').matchesQuery('taskList', taskListQuery).each(async (submission) => {
+  //     await submission.destroy({ useMasterKey: true })
+  //   }, { useMasterKey: true })
+  //   await taskListQuery.each(async (taskList) => {
+  //     await taskList.destroy({ useMasterKey: true })
+  //   }, { useMasterKey: true })
+  //   await control.destroy({ useMasterKey: true })
+  //   console.info('Control removed')
+  // }, { useMasterKey: true })
 
-  // disassemblies
-  await $query('Contract').containedIn('objectId', []).each(async (contract) => {
-    // delete all submissions
-    const disassembliesQuery = $query('Disassembly').equalTo('contract', contract)
-    const taskListQuery = $query('TaskList').matchesQuery('disassembly', disassembliesQuery)
-    await $query('DisassemblySubmission').matchesQuery('taskList', taskListQuery).each(async (submission) => {
-      await submission.destroy({ useMasterKey: true })
-    }, { useMasterKey: true })
-    await taskListQuery.each(async (taskList) => {
-      await taskList.destroy({ useMasterKey: true })
-    }, { useMasterKey: true })
-    await disassembliesQuery.each(async (disassembly) => {
-      await disassembly.destroy({ useMasterKey: true })
-    }, { useMasterKey: true })
-    await contract.set('status', 2).save(null, { useMasterKey: true })
-    await contract.destroy({ useMasterKey: true })
-    console.info('Contract removed')
-  }, { useMasterKey: true })
+  // disassembly test contracts
+  // await $query('Contract').containedIn('objectId', []).each(async (contract) => {
+  //   // delete all submissions
+  //   const disassembliesQuery = $query('Disassembly').equalTo('contract', contract)
+  //   const taskListQuery = $query('TaskList').matchesQuery('disassembly', disassembliesQuery)
+  //   await $query('DisassemblySubmission').matchesQuery('taskList', taskListQuery).each(async (submission) => {
+  //     await submission.destroy({ useMasterKey: true })
+  //   }, { useMasterKey: true })
+  //   await taskListQuery.each(async (taskList) => {
+  //     await taskList.destroy({ useMasterKey: true })
+  //   }, { useMasterKey: true })
+  //   await disassembliesQuery.each(async (disassembly) => {
+  //     await disassembly.destroy({ useMasterKey: true })
+  //   }, { useMasterKey: true })
+  //   await contract.set('status', 2).save(null, { useMasterKey: true })
+  //   await contract.destroy({ useMasterKey: true })
+  //   console.info('Contract removed')
+  // }, { useMasterKey: true })
 }
 
 require('./run')(clean)
