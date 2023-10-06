f04dafba0a683dc509812abaec9b2c4cc3f680dc
isokosan <denizar@gmail.com>
2023-10-06 12:14:08 +0200

    fix: booking requests skip bug in search index

--- src/jobs/reindex_booking_requests/index.js
+++ src/jobs/reindex_booking_requests/index.js
@@ -6,21 +6,17 @@ module.exports = async function (job) {
   const query = INDEXES[index].parseQuery
   const total = await query.count({ useMasterKey: true })
   let i = 0
-  while (true) {
-    query.skip(i)
-    const body = await query.find({ useMasterKey: true })
-      .then(INDEXES[index].datasetMap)
-      .then(dataset => dataset.flatMap(({ doc, _id }) => [{ index: { _index: index, _id } }, doc]))
-    if (!body.length) {
-      break
-    }
+  await query.eachBatch(async (results) => {
+    const dataset = await INDEXES[index].datasetMap(results)
+    const body = dataset.flatMap(({ doc, _id }) => [{ index: { _index: index, _id } }, doc])
     const { items } = await client.bulk({ refresh: true, body })
     const error = items.find(item => item.index.error)
     error && consola.error(error.index.error)
-    i += items.length
+    i += results.length
     const progress = parseInt(100 * i / total)
     job && job.progress(progress)
-  }
+  }, { useMasterKey: true })
+
   const { count } = await client.count({ index })
   return `${count} items saved in ${index}`
 }
