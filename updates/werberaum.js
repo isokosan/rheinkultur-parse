7dcacf64b99eb99a50467a57b4eaf463ed28b45f
isokosan <denizar@gmail.com>
2023-10-11 09:44:15 +0200

    chore: werberaum match

--- updates/werberaum.js
+++ updates/werberaum.js
@@ -1,5 +1,58 @@
+const DOUBLES = [
+  'B22-0881',
+  'B23-0195',
+  'B22-0697',
+  'B22-1019',
+  'B19-0416',
+  'B19-0416',
+  'B22-0854',
+  'B23-0620',
+  'B23-0516',
+  'B23-0209',
+  'B20-0186',
+  'B20-0197',
+  'B22-0583',
+  'B20-0197',
+  'B20-0186',
+  'B23-0400',
+  'B20-0547',
+  'B20-0547',
+  'B21-0122',
+  'B21-0122',
+  'B23-0645',
+  'B23-0016',
+  'B23-0663',
+  'B22-0853',
+  'B22-0362',
+  'B22-0362',
+  'B22-0583',
+  'B22-0697',
+  'B22-0945',
+  'B23-0213',
+  'B23-0221',
+  'B22-0853',
+  'B22-0854',
+  'B22-0881',
+  'B22-0945',
+  'B22-1019',
+  'B23-0016',
+  'B23-0195',
+  'B23-0237',
+  'B23-0209',
+  'B23-0213',
+  'B23-0221',
+  'B23-0237',
+  'B23-0551',
+  'B23-0400',
+  'B23-0516',
+  'B23-0551',
+  'B23-0620',
+  'B23-0645',
+  'B23-0663'
+]
+
 require('./run')(async () => {
-  const fs = require('fs').promises
+  // const fs = require('fs').promises
   const csv = require('csvtojson')
   const csvFilePath = require('path').resolve(__dirname, 'werberaum.csv')
   const all = await csv({ trim: true }).fromFile(csvFilePath)
@@ -13,6 +66,7 @@ require('./run')(async () => {
     .select('objectId', 'order', 'futureOrder')
     .find({ useMasterKey: true })
 
+  let i = 0
   const response = []
   for (const row of all) {
     const cube = cubes.find(cube => cube.id === row.objectId)
@@ -25,12 +79,23 @@ require('./run')(async () => {
           ? cube.get('futureOrder')
           : undefined
       row.match = order?.no
+      if (row.match && !DOUBLES.includes(row.match)) {
+        const booking = await $query('Booking').equalTo('no', row.match).first({ useMasterKey: true })
+        row.externalOrderNo = row.externalOrderNo.trim()
+        if (booking && booking.get('externalOrderNo') !== row.externalOrderNo) {
+          console.log(booking.get('no'), row.externalOrderNo)
+          booking.set('externalOrderNo', row.externalOrderNo)
+          await booking.save(null, { useMasterKey: true })
+          i++
+        }
+      }
     }
     if (!row.match) {
       row.problem = cube ? 'Buchung nicht gefunden' : 'Cube nicht gefunden'
     }
     response.push(row)
   }
+  console.log('DONE', i)
   // save response as werberaum.json
-  return fs.writeFile('./updates/werberaum.json', JSON.stringify(response))
+  // return fs.writeFile('./updates/werberaum.json', JSON.stringify(response))
 })
