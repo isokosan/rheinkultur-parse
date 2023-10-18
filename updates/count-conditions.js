9b713feffaab8f2647a9376d4dcb8a120893f885
isokosan <denizar@gmail.com>
2023-10-18 15:54:38 +0200

    chore: lint

--- updates/count-conditions.js
+++ updates/count-conditions.js
@@ -1,4 +1,4 @@
-require('./run')(async() => {
+require('./run')(async () => {
   await $query('Contract').equalTo('pricingModel', 'fixed').aggregate([
     { $group: { _id: '$company', count: { $sum: 1 } } }
   ]).then(console.log)
