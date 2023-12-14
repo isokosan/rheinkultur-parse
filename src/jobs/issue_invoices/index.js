2a5a1ddb708701bd85f630188a7699a0196a0953
isokosan <denizar@gmail.com>
2023-12-14 12:39:23 +0100

    fix: skipInvoiceEmails checks in issue-invoices and send-issued-invoice-emails jobs

--- src/jobs/issue_invoices/index.js
+++ src/jobs/issue_invoices/index.js
@@ -4,6 +4,7 @@ module.exports = async function (job) {
   const query = $query('Invoice')
     .equalTo('lexId', null)
     .equalTo('status', 1)
+    .include('contract')
     .lessThanOrEqualTo('date', await $today())
   const total = await query.count({ useMasterKey: true })
   let i = 0
@@ -12,8 +13,14 @@ module.exports = async function (job) {
     if (!invoice) {
       return Promise.resolve({ issuedInvoices: i })
     }
+    // TODO: remove the email setter here and revert to email: !DEVELOPMENT after adding checks in invoices.js
+    // Then you can remove the include contract above and only fetch ids
+    let email = !DEVELOPMENT
+    if (invoice.get('contract')?.get('skipInvoiceEmails')) {
+      email = false
+    }
     // when email is true the email from invoice address will be used
-    await Parse.Cloud.run('invoice-issue', { id: invoice.id, email: !DEVELOPMENT }, { useMasterKey: true })
+    await Parse.Cloud.run('invoice-issue', { id: invoice.id, email }, { useMasterKey: true })
     // wait 2 seconds
     await new Promise(resolve => setTimeout(resolve, 2000))
     consola.info('issued invoice', invoice.id)
