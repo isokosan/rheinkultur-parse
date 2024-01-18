86fab4b700318780bfdece4f461de5c12be1e10b
isokosan <denizar@gmail.com>
2024-01-18 16:06:28 +0100

    chore: add update permissions

--- updates/playground.js
+++ updates/playground.js
@@ -1,14 +1,22 @@
 require('./run')(async () => {
-  const unique = []
-  await $query('TaskList').select(['statuses']).eachBatch(async (batch) => {
-    for (const item of batch) {
-      const array = [...new Set(Object.values(item.get('statuses')))]
-      for (const status of array) {
-        if (!unique.includes(status)) {
-          unique.push(status)
+  let i = 0
+  await $query(Parse.User)
+    .containedIn('objectId', ['YxuxJiEq7g', 'PALgvoyg3O'])
+    .each(async (user) => {
+      const permissions = user.get('permissions')
+      let changed = false
+      for (const item of ['manage-scouts', 'manage-fieldwork']) {
+        if (!permissions.includes(item)) {
+          permissions.push(item)
+          changed = true
         }
       }
-    }
-  }, { useMasterKey: true, batchSize: 1000 })
-  console.log(unique)
+      if (changed) {
+        await user.set({ permissions }).save(null, { useMasterKey: true })
+        i++
+      }
+      console.log(user.get('firstName'), permissions)
+    }, { useMasterKey: true })
+  console.log(i)
+  return i
 })
