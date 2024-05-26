6b6151576ec676cabe6f97e578473fdb63553b6f
isokosan <denizar@gmail.com>
2024-05-26 16:57:32 +0200

    chore: update deps

--- updates/playground.js
+++ updates/playground.js
@@ -1,11 +1,23 @@
 require('./run')(async () => {
-  for (const parent of ['Briefing', 'Control', 'Assembly', 'Disassembly', 'CustomService']) {
-    let i = 0
-    await $query(parent).each(async (item) => {
-      await item.save(null, { useMasterKey: true, context: { syncStatus: true } })
+  let i = 0
+  await $query('Invoice')
+    .equalTo('date', '2024-05-18')
+    .equalTo('status', 2)
+    .equalTo('lexId', null)
+    .equalTo('lexUri', null)
+    .equalTo('lexNo', null)
+    .each(async (invoice) => {
+      if (invoice.get('lexId')) {
+        console.log('Invoice already has lexId', invoice.id)
+        return
+      }
+      invoice
+        .set('status', 1)
+        .set('date', '2024-05-21')
+        .unset('voucherDate')
+      await invoice.save(null, { useMasterKey: true })
       i++
-      console.log('Updated', parent, i)
     }, { useMasterKey: true })
-  }
-  console.log('Done')
+  console.log('OK')
+  console.log(i)
 })
